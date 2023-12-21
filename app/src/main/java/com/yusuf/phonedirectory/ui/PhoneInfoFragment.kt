c4e0dfe1857ecec80738147155e8aac085138006
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-12-21 16:09:11 +0300

    UI

--- app/src/main/java/com/yusuf/phonedirectory/ui/PhoneInfoFragment.kt
+++ app/src/main/java/com/yusuf/phonedirectory/ui/PhoneInfoFragment.kt
@@ -1,23 +1,45 @@
 package com.yusuf.phonedirectory.ui
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.navArgs
 import com.yusuf.phonedirectory.R
+import com.yusuf.phonedirectory.databinding.FragmentPhoneInfoBinding
 
 class PhoneInfoFragment : Fragment() {
 
+    private lateinit var binding: FragmentPhoneInfoBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
-        return inflater.inflate(R.layout.fragment_phone_info, container, false)
+        binding = FragmentPhoneInfoBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val bundle : PhoneInfoFragmentArgs by navArgs()
+
+        when (bundle.info){
+            0 -> {
+                binding.Save.visibility = Button.VISIBLE
+                binding.editTextContactName.isEnabled = true
+                binding.toolbarPhoneInfo.isEnabled = true
+            }
+
+            1 -> {
+                binding.Save.visibility = Button.INVISIBLE
+                binding.editTextContactName.isEnabled = false
+                binding.toolbarPhoneInfo.isEnabled = false
+            }
+
+        }
     }
 
 }
