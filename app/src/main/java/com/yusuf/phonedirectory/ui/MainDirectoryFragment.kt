c5ef1a63e8dd1f7e4da7dfa6f9ce558ff28d7349
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-12-21 16:08:28 +0300

    Nav Args Update

--- app/src/main/java/com/yusuf/phonedirectory/ui/MainDirectoryFragment.kt
+++ app/src/main/java/com/yusuf/phonedirectory/ui/MainDirectoryFragment.kt
@@ -5,21 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yusuf.phonedirectory.R
+import com.yusuf.phonedirectory.data.entity.Kisiler
+import com.yusuf.phonedirectory.databinding.FragmentMainDirectoryBinding
 
 
 class MainDirectoryFragment : Fragment() {
 
 
+    private lateinit var binding: FragmentMainDirectoryBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_directory, container, false)
+        binding = FragmentMainDirectoryBinding.inflate(inflater, container, false)
+
+
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        (activity as? AppCompatActivity)?.setSupportActionBar(binding.toolbar)
+
+        // Toolbar'ın yüksekliğini ayarlayın.
+        //binding.toolbar.layoutParams?.height = resources.getDimensionPixelSize(R.dimen.toolbar_height)
+
+
+        binding.mainDirectoryRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        //adapter
+
+        binding.fab.setOnClickListener {
+            val action = MainDirectoryFragmentDirections.actionMainDirectoryFragmentToPhoneInfoFragment(
+                Kisiler(0,"",""),0)
+            findNavController().navigate(action)
+        }
+
+
     }
+
+
 }
