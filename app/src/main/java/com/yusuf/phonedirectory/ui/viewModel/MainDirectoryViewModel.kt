297764a02dd1f6aa5a59012db325e24e2f5df280
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-12-22 16:23:52 +0300

    Repo Connected

--- app/src/main/java/com/yusuf/phonedirectory/ui/viewModel/MainDirectoryViewModel.kt
+++ app/src/main/java/com/yusuf/phonedirectory/ui/viewModel/MainDirectoryViewModel.kt
@@ -1,8 +1,9 @@
 package com.yusuf.phonedirectory.ui.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.yusuf.phonedirectory.data.repository.ContactRepository
 
 class MainDirectoryViewModel : ViewModel() {
 
-
+    val repo = ContactRepository()
 }
--- app/src/main/java/com/yusuf/phonedirectory/ui/viewModel/PhoneInfoViewModel.kt
+++ app/src/main/java/com/yusuf/phonedirectory/ui/viewModel/PhoneInfoViewModel.kt
@@ -1,8 +1,19 @@
 package com.yusuf.phonedirectory.ui.viewModel
 
 import androidx.lifecycle.ViewModel
+import com.yusuf.phonedirectory.data.repository.ContactRepository
 
 class PhoneInfoViewModel : ViewModel() {
 
+    private var repo = ContactRepository()
+
+    fun saveContact(contactName: String, contactNumber: String){
+        repo.saveContact(contactName, contactNumber)
+    }
+
+
+    fun updateContact(contactName: String, contactNumber: String){
+        repo.updateContact(contactName, contactNumber)
+    }
 
 }
